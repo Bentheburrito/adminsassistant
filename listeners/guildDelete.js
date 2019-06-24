28e900d6a8b6847cab596391fc97c5938fe75aeb
fred <fredsterdickinson@hotmail.com>
2019-06-24 22:31:20 +0100

    Added ./listeners/guildDelete.js

---
+++ listeners/guildDelete.js
@@ -0,0 +1,6 @@
+module.exports = (client, guild) => {
+    console.log(`The client has been removed from the guild ${guild.name}, or the guild was deleted.`);
+
+    // Same w/ guildCreate, if we are storing guild data then it can be deleted:
+    // ~~~
+}
