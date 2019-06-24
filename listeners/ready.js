20f4805134dbda74a0f6cc854a6c056fd2d039ee
fred <fredsterdickinson@hotmail.com>
2019-06-24 22:06:19 +0100

    Added ./ready.js

---
+++ listeners/ready.js
@@ -0,0 +1,7 @@
+exports.run = (client) => {
+    console.log(`Successfully logged in as ${client.user.tag}, serving ${client.guilds.size} guilds!`);
+    client.user.setActivity("Now Online", { type: "PLAYING"} );
+
+    // In the future we may need to call some funcs/other things here, otherwise it will pb be mainly empty:
+    // ~~~
+}
